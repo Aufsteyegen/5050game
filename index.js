9cb67f52a547971cef2a0add4d30a061fddea08b
Greg <90116441+Aufsteyegen@users.noreply.github.com>
2022-08-29 22:48:58 -0500

    Add files via upload
    Smooth hover animation on main page for input. Added (non-working) status indicator. Added rotating three.js mesh to navbar (untextured).

--- index.js
+++ index.js
@@ -6,21 +6,25 @@ import * as dat from 'dat.gui'
 // Debug
 const gui = new dat.GUI()
 
+// GLTF mesh loading
 const loader = new GLTFLoader();
 
-loader.load( 'ATTEMPT3.glb',  gltf => {
 
-	scene.add( gltf.scene );
+loader.load( '29tst.glb',  gltf => {
 
-    gui.add(gltf.scene.rotation, 'y').min(0).max(9)
-    gui.add(gltf.scene.rotation, 'x').min(0).max(9)
-    gui.add(gltf.scene.rotation, 'z').min(0).max(9)
+    model = gltf.scene
+	scene.add( model )
+
+    gui.add(model.rotation, 'y').min(5).max(9)
+    gui.add(model.rotation, 'x').min(0).max(9)
+    gui.add(model.rotation, 'z').min(0).max(9)
 
 }, undefined, function ( error ) {
 
 	console.error( error );
 
 } );
+
 
 // Canvas
 const canvas = document.querySelector('#upperq')
@@ -29,86 +33,34 @@ const canvas = document.querySelector('#upperq')
 const scene = new THREE.Scene()
 
 // Lights
+const light = new THREE.DirectionalLight('white', 55)
+light.position.set(0, 0.8, 1)
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+scene.add(light)
 
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+//Camera
+var camera = new THREE.PerspectiveCamera( 105, window.innerWidth/window.innerHeight, 0.1, 1000 );
+scene.add(camera);
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 0.25
+camera.position.y = 0.2
 camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
 
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
 	alpha: true
 })
-renderer.setSize(sizes.width, sizes.height)
+
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+function animate() {
+    requestAnimationFrame( animate );
 
-var hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-hemiLight.position.set( 0, 0, 0 );
-scene.add( hemiLight );
-renderer.outputEncoding = THREE.sRGBEncoding;
+    model.rotation.y += 0.01;
+    
 
-/**
- * Animate
- */
+    renderer.render( scene, camera );
+};
 
-const clock = new THREE.Clock()
-
-const tick = () =>
-{
-
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    // sphere.rotation.y = .5 * elapsedTime
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
+animate();
