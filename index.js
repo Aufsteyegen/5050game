75afa331a236a49a93bd6b0034007b72bbe1b326
Greg <90116441+Aufsteyegen@users.noreply.github.com>
2022-09-05 06:17:09 -0500

    Add files via upload
    Changes html layout, play button, and starts react-three-fiber integration.

--- index.js
+++ index.js
@@ -1,66 +1,37 @@
-import '../CSS/main.css'
-import * as THREE from 'three'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import * as dat from 'dat.gui'
+import { React, Suspense }  from 'react'
+import ReactDOM from 'react-dom'
+import { Canvas } from '@react-three/fiber'
+import { useGLTF } from '@react-three/drei'
 
-// Debug
-const gui = new dat.GUI()
+function Model(props) {
+    const { nodes, materials } = useGLTF("/29tst.glb");
+    return (
+      <group {...props} dispose={null}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Text.geometry}
+          material={materials.Glitter}
+          position={[0, 0.22, -0.02]}
+          rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+          scale={0.75}
+        />
+      </group>
+  );
+}
 
-// GLTF mesh loading
-const loader = new GLTFLoader();
-
-
-loader.load( '29tst.glb',  gltf => {
-
-    model = gltf.scene
-	scene.add( model )
-
-    gui.add(model.rotation, 'y').min(5).max(9)
-    gui.add(model.rotation, 'x').min(0).max(9)
-    gui.add(model.rotation, 'z').min(0).max(9)
-
-}, undefined, function ( error ) {
-
-	console.error( error );
-
-} );
-
-
-// Canvas
-const canvas = document.querySelector('#upperq')
-
-// Scene
-const scene = new THREE.Scene()
-
-// Lights
-const light = new THREE.DirectionalLight('white', 55)
-light.position.set(0, 0.8, 1)
-
-scene.add(light)
-
-//Camera
-var camera = new THREE.PerspectiveCamera( 105, window.innerWidth/window.innerHeight, 0.1, 1000 );
-scene.add(camera);
-
-camera.position.z = 0.25
-camera.position.y = 0.2
-camera.position.x = 0
-
-// Renderer
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-	alpha: true
-})
-
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-function animate() {
-    requestAnimationFrame( animate );
-
-    model.rotation.y += 0.01;
-    
-
-    renderer.render( scene, camera );
-};
-
-animate();
+function App() {
+    return (
+      <span id="canvas-container">
+        <Canvas>
+        <Suspense fallback={null}>
+            <ambientLight intensity={0.1} />
+            <directionalLight color="red" position={[0, 0, 5]} />
+            <Model />
+            </Suspense>
+        </Canvas>
+      </span>
+    )
+  }
+  
+  ReactDOM.render(<App />, document.getElementById('playing'))
